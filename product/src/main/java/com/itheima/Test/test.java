cea2238ccef449d40b0b24ce14fbda509835ac70
hj <huangjian@itcast.cn>
2019-02-24 16:34:57 +0800

    更新

--- product/src/main/java/com/itheima/Test/test.java
+++ product/src/main/java/com/itheima/Test/test.java
@@ -3,6 +3,5 @@ package com.itheima.Test;
 public class test {
     public static void main(String[] args) {
         System.out.println("1111111111111111");
-        System.out.println("2222222222222222");
     }
 }
